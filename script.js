d8c051883f13109185de32934b344207ddf19d17
Thea Blachon <thea.blachon73@gmail.com>
2023-03-19 11:07:41 +0100

    :sparkles:adding menu burger

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ meeting.addEventListener("mouseover", function() {
 meeting.addEventListener("mouseout", function() {
     meeting.setAttribute('src', 'img/photo-accueil-cabinet-meeting.png')
 });
+
+
+if(screen.width <= 640) {
+
+    function $(cible){
+        return document.querySelectorAll(cible)[0];
+    }
+    
+    let img = document.querySelector(".img-burger");
+    let div = document.querySelector("#menu");
+    
+    
+    function menuOuvre(evt) { 
+    
+        div.style.right = "10%";
+        div.style.display = "flex";
+        evt.stopPropagation();
+    }
+    
+    function menuFerme() {
+        div.style.right = "-200px"
+        div.style.display = "none";
+    }
+    
+    function stop(evt) {
+        evt.stopPropagation();
+    }
+
+    img.addEventListener("click", menuOuvre, false);
+    window.addEventListener("click", menuFerme, false);
+    img.addEventListener("click", stop, false)
+}
